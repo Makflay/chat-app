87670e5827cf9e366f1005387fb8caf62ba0c67c
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-04-21 18:23:38 +0300

    feat: update app entry point and integrate routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import AppRouter from "./routes/AppRouter";
 import { useAuthStore } from "./store/auth-store";
 
 function App() {
-  const initAuth = useAuthStore((state) => state.initAuth);
+  const isInitialized = useAuthStore((state) => state.isInitialized);
+  const fetchMe = useAuthStore((state) => state.fetchMe);
+  const setInitialized = useAuthStore((state) => state.setInitialized);
+
+  const startedRef = useRef(false);
 
   useEffect(() => {
-    initAuth();
-  }, [initAuth]);
+    const initAuth = async () => {
+      await useAuthStore.persist.rehydrate();
+
+      const { token } = useAuthStore.getState();
+      console.log("token useEffect App", token);
+
+      if (!token) {
+        setInitialized(true);
+        return;
+      }
+
+      await fetchMe();
+    };
+    console.log("startedRef.current", startedRef.current);
+    if (startedRef.current) return;
+    startedRef.current = true;
+
+    void initAuth();
+  }, [fetchMe, setInitialized]);
+
+  console.log("isInitialized App.tsx", isInitialized);
+
+  if (!isInitialized) {
+    return <p>Loading...</p>;
+  }
 
   return <AppRouter />;
 }
